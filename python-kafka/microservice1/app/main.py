361d6d7bcb5e8950e18031be9055e92aa59faf69
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-05-18 20:46:04 +0500

    update: kafka and python (fastapi) using aiokafka playground

--- python-kafka/microservice1/app/main.py
+++ python-kafka/microservice1/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from sqlmodel import SQLModel, Field
-from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
-from app import settings
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer, TopicPartition
+from contextlib import asynccontextmanager
 import logging
 import asyncio
+
+from app import settings
+from app.topic import startup_topic_event
+from app.utils import key_deserializer, value_deserializer
+
 # Logging configuration
 logging.basicConfig(level=logging.INFO)
 
@@ -14,8 +19,16 @@ class BookOrder(SQLModel):
     user_id: int
     price: float
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.info("Starting application")    
+    # THIS WILL FAIL DUE TO BROKER VERSION COMPAITIBILITY
+    # ERROR: IncompatibleBrokerVersion: Kafka broker does not support the 'CreateTopicsRequest_v0' Kafka protocol.
+    # asyncio.create_task(startup_topic_event())
+    logging.info("Application started")
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 @app.post("/order-book")
 async def order_book(book_order: BookOrder):
@@ -45,21 +58,57 @@ async def consume_messages():
     consumer = AIOKafkaConsumer(
         settings.KAFKA_BOOK_ORDER_TOPIC,
         bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVER,
-        group_id=settings.KAFKA_CONSUMER_BOOK_GROUD_ID
+        group_id=settings.KAFKA_CONSUMER_BOOK_GROUD_ID,
+        key_deserializer=key_deserializer,
+        value_deserializer=value_deserializer,
+        auto_offset_reset='latest',  # Start reading from the earliest offset if no offset is committed
     )
     await consumer.start()
 
     try:
-        # msg = await consumer.getone()
+        # Fetch a single message with a timeout
         msg = await asyncio.wait_for(consumer.getone(), timeout=10.0)
-        print(
+        tp = TopicPartition(msg.topic, msg.partition)
+
+        # Current position
+        position = await consumer.position(tp)
+        logging.info(f"Current position: {position}")
+        
+        # Committed offset
+        committed = await consumer.committed(tp)
+        logging.info(f"Committed offset: {committed}")
+
+        logging.info(
             "{}:{:d}:{:d}: key={} value={} timestamp_ms={}".format(
                 msg.topic, msg.partition, msg.offset, msg.key, msg.value,
                 msg.timestamp)
         )
-        return {"raw_message": msg, "value": msg.value.decode('utf-8')}
+        return {"raw_message": msg, "offset": msg.offset, "key": msg.key, "value": msg.value}
     except asyncio.TimeoutError:
+        logging.warning("No messages received within the timeout period")
         raise HTTPException(status_code=408, detail="No messages received within the timeout period")
     finally:
-        logging.info("Stopping consumer")
+        logging.info("Finally Stopping consumer")
         await consumer.stop()
+        
+@app.post("/get-food")
+async def get_food(food_name: str):
+    producer = AIOKafkaProducer(
+        bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVER,
+        acks="all",
+    )
+    logging.info(f"KAFKA_BOOTSTRAP_SERVER: {settings.KAFKA_BOOTSTRAP_SERVER}")
+    logging.info(f"Food order received: {food_name}")
+    
+    await producer.start()
+    
+    try:
+        await producer.send_and_wait(
+            topic=settings.KAFKA_FOOD_ORDER_TOPIC,
+            value=food_name.encode('utf-8'),
+            key=food_name.encode('utf-8'),
+        )
+    finally:
+        await producer.stop()
+
+    return {"food_name": food_name}
